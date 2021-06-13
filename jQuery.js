601d37596d86c53a9562008cf337fefd86bdbcda
Lazar Jordanovic <75857294+Lazartarzan@users.noreply.github.com>
2021-06-13 19:21:08 +0200

    Update jQuery.js

--- jQuery.js
+++ jQuery.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $(".btn").click(function(){
       $(".broj").remove();
       $(".brojevi").remove();
-      setInterval(getBroj,3000);
+//       setInterval(getBroj,3000);
     });
   });
   
